002a4cd5c3ee71f49f982a4bf59a1f985f4b2b8c
Right9lt <bytethe@gmail.com>
2022-01-19 11:40:26 +0200

    Password hash

--- UsersLibrary/User.cs
+++ UsersLibrary/User.cs
@@ -3,17 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace UsersLibrary
 {
     public class User
     {
-        public string _connectionString = PasswordManager.Default.BayrueConnectionString;
-
         public int Id { get; private set; }
         public string Email { get; set; }
-        public string Password { get; set; }
-        public string Salt { get; set; }f
+        private string _password;
+        public string Password
+        {
+            get => _password; set
+            {
+                Random random = new Random();
+                StringBuilder salt = new StringBuilder();
+                for (int i = 0; i < 20; i++)
+                {
+                    salt.Append((char)random.Next(65, 90));
+                }
+                _password = string.Concat(_password, salt);
+                using (SHA256 sHA256 = SHA256.Create())
+                {
+                    byte[] hash = sHA256.ComputeHash(Encoding.UTF8.GetBytes(_password));
+                    StringBuilder sb = new StringBuilder();
+                    foreach (var item in hash)
+                    {
+                        sb.Append(item.ToString("x2"));
+                    }
+                    _password = sb.ToString();
+                }
+            }
+        }
+        public string Salt { get; set; }
     }
 
 }
