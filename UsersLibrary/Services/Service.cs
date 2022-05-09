0d76a22e2da6ddb3af74172c6145b257f1fa12eb
YourAndrew101 <andrewratushnyak0@gmail.com>
2022-05-09 14:47:45 +0300

    Normal commit

---
+++ UsersLibrary/Services/Service.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersLibrary.Services
+{
+    public class Service
+    {
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public Service(string name, string login, string password)
+        {
+            Name = name;
+            Login = login;
+            Password = password;
+        }
+    }
+}
---
+++ UsersLibrary/Settings/SignUpSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersLibrary.Settings
+{
+    public class SignUpSettings
+    {
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+
+        public SignUpSettings(User user)
+        {
+            Email = user.Email;
+            Password = user.AuthPassword;
+        }
+
+        public SignUpSettings() { }
+
+        public SignUpSettings(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+    }
+}
