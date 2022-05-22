302dadb63ab9a4c3f6c78a2199f23293e332f649
YourAndrew101 <andrewratushnyak0@gmail.com>
2022-05-22 18:56:40 +0300

    Add DataProviders factories

---
+++ Data/DataProviders/Factories/IDataProviderFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.DataProviders.Products;
+using ServicesLibrary;
+using UsersLibrary;
+
+namespace Data.DataProviders.Factories
+{
+    public abstract class IDataProviderFactory
+    {
+        public abstract IDataProvider GetDataProvider();
+
+        public IDataProviderFactory[] CreateFactory()
+        {
+            if (InternetService.IsConnectedToInternet)
+                return new IDataProviderFactory[] { new SQLDataProviderFactory(), new XMLDataProviderFactory() };
+
+            return new IDataProviderFactory[] { new XMLDataProviderFactory()};
+        }
+    }
+}
